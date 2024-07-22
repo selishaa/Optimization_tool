8e28a1237bbf01bf148fc9858c412143291cca14
Selisha <selishathapa5@gmail.com>
2024-07-22 02:41:09 -0400

    added comparison page

--- pages/views.py
+++ pages/views.py
@@ -58,6 +58,9 @@ session_data = {
     'cattle': None
 }
 
+def comparison(request):
+    return render(request, 'comparison.html')
+
 def your_form_submission_view(request):
     if request.method == 'POST':
         # Process the form data here
@@ -68,6 +71,7 @@ def your_form_submission_view(request):
         
         return redirect('optimization_tool')  # Redirect to the next view
     return render(request, 'demo.html', {})
+
 
 
 
@@ -83,6 +87,8 @@ def plot_view(request):
 def results_view(request):
      results = request.session.get('results', {})
      return render(request, 'result_page.html', results)
+ 
+ 
 
 
 #calls the functions in SIRES with the given values input by the user
@@ -231,208 +237,6 @@ def calculations_view(request):
    
 
 
-    # def results_view(request):
-    #     results = request.session.get('results', {})
-        
-    #     # Assuming chart1, chart2, ... are base64 encoded strings in results
-    #     chart1 = results.get('chart1', None)
-    #     chart2 = results.get('chart2', None)
-    #     chart3 = results.get('chart3', None)
-    #     chart4 = results.get('chart4', None)
-    #     chart5 = results.get('chart5', None)
-
-    #     # Decode charts if they are present
-    #     # Decode the base64 strings if they exist
-    #     if chart1:
-    #         try:
-    #             chart1_decoded = base64.b64decode(chart1).decode('utf-8')
-    #         except Exception as e:
-    #             print(f"Error decoding chart1: {e}")
-    #             chart1_decoded = None
-    #     else:
-    #         chart1_decoded = None
-
-    #     if chart2:
-    #         try:
-    #             chart2_decoded = base64.b64decode(chart2).decode('utf-8')
-    #         except Exception as e:
-    #             print(f"Error decoding chart2: {e}")
-    #             chart2_decoded = None
-    #     else:
-    #         chart2_decoded = None
-
-    #     if chart3:
-    #         try:
-    #             chart3_decoded = base64.b64decode(chart3).decode('utf-8')
-    #         except Exception as e:
-    #             print(f"Error decoding chart3: {e}")
-    #             chart3_decoded = None
-    #     else:
-    #         chart3_decoded = None
-
-    #     if chart4:
-    #         try:
-    #             chart4_decoded = base64.b64decode(chart4).decode('utf-8')
-    #         except Exception as e:
-    #             print(f"Error decoding chart4: {e}")
-    #             chart4_decoded = None
-    #     else:
-    #         chart4_decoded = None
-
-    #     if chart5:
-    #         try:
-    #             chart5_decoded = base64.b64decode(chart5).decode('utf-8')
-    #         except Exception as e:
-    #             print(f"Error decoding chart5: {e}")
-    #             chart5_decoded = None
-    #     else:
-    #         chart5_decoded = None
-
-    #     return render(request, 'result_page.html', {
-    #         'solar_energy': results.get('solar_energy', None),
-    #         'solar': results.get('solar', None),
-    #         'wind_energy': results.get('wind_energy', None),
-    #         'wind': results.get('wind', None),
-    #         'hydro_energy': results.get('hydro_energy', None),
-    #         'hydro': results.get('hydro', None),
-    #         'bio_energy': results.get('bio_energy', None),
-    #         'bio': results.get('bio', None),
-    #         'solar_ind': results.get('solar_ind', None),
-    #         'wind_ind': results.get('wind_ind', None),
-    #         'hydropower_ind': results.get('hydropower_ind', None),
-    #         'biogas_ind': results.get('biogas_ind', None),
-    #         'battery_ind': results.get('battery_ind', None),
-    #         'biogasWP_ind': results.get('biogasWP_ind', None),
-    #         'windWP_ind': results.get('windWP_ind', None),
-    #         'solarWP_ind': results.get('solarWP_ind', None),
-    #         'LPSP': results.get('LPSP', None),
-    #         'LWSP': results.get('LWSP', None),
-    #         'chart1': chart1_decoded,
-    #         'chart2': chart2_decoded,
-    #         'chart3': chart3_decoded,
-    #         'chart4': chart4_decoded,
-    #         'chart5': chart5_decoded,
-    #     })
-        
-    
-# def calculations_view(request):
-#     if request.method == 'POST':
-#         print("Post request at calculations_view is working")
-
- 
-#         solar = str(request.POST.get("solar"))
-#         if solar == "sunny":
-#             G = [0, 0, 0, 0, 0, 1, 93, 273, 470, 664, 819, 932, 975, 992, 762, 820, 598, 459, 261, 25, 0, 0, 0, 0]
-#         elif solar == "partly_sunny":
-#             G = [0, 0, 0, 0, 0, 0, 0, 9, 310, 95, 353, 646, 692, 675, 590, 458, 300, 77, 1, 0, 0, 0, 0, 0]
-#         elif solar == "cloudy":
-#             G = [0, 0, 0, 0, 0, 0, 0, 13, 144, 331, 527, 425, 361, 392, 288, 212, 144, 79, 24, 0, 0, 0, 0, 0]
-#         else:
-#             G = []
-#         set_solar(G)
-#         solar_energy = get_solar()
-
-#         wind = str(request.POST.get("wind"))
-#         if wind == "high_wind":
-#             v = [7, 6, 9, 9, 11, 10, 10, 11, 18, 15, 14, 15, 11, 16, 15, 14, 15, 13, 16, 14, 16, 15, 13, 12]
-#         elif wind == "medium_wind":
-#             v = [10, 9, 8, 7, 6, 7, 5, 9, 7, 10, 9, 10, 9, 7, 6, 6, 8, 7, 5, 4, 1, 1, 1, 2]
-#         elif wind == "low_wind":
-#             v = [5, 2, 1, 1, 0, 1, 1, 2, 3, 6, 6, 4, 4, 3, 5, 5, 1, 6, 2, 5, 2, 5, 4, 4]
-#         else:
-#             v = []
-#         set_wind(v)
-#         wind_energy = get_wind()
-
-#         lakes = 'lakes' in request.POST
-#         rivers = 'rivers' in request.POST
-#         ponds = 'ponds' in request.POST
-
-#         if rivers and lakes and ponds:
-#             base_h = 35
-#             hydro = "lakes, rivers and ponds"
-#         elif rivers and lakes and not ponds:
-#             base_h = 25
-#             hydro = "lakes and rivers"
-#         elif not rivers and lakes and ponds:
-#             base_h = 10
-#             hydro = "lakes and ponds"
-#         elif rivers and not lakes and not ponds:
-#             base_h = 20
-#             hydro = "rivers"
-#         elif lakes and not rivers and not ponds:
-#             base_h = 5
-#             hydro = "lakes"
-#         elif ponds and not rivers and not lakes:
-#             base_h = 2
-#             hydro = "ponds"
-#         else:
-#             base_h = 0
-#             hydro = ""
-#         set_hydro(base_h)
-#         hydro_energy = get_hydro()
-
-#         cattle = 'cattle' in request.POST
-#         agriculture = 'agriculture' in request.POST
-
-#         if cattle and agriculture:
-#             base_b = 2
-#             bio = "cattle and agriculture"
-#         elif cattle or agriculture:
-#             base_b = 1
-#             bio = "cattle or agriculture"
-#         else:
-#             base_b = 0
-#             bio = ""
-#         set_bio(base_b)
-#         bio_energy = get_bio()
-
-#         mainYear()
-#         individual = bestYear()
-#         solar_ind = individual[0]
-#         wind_ind = individual[1]
-#         hydropower_ind = individual[2]
-#         biogas_ind = individual[3]
-#         battery_ind = individual[4]
-#         biogasWP_ind = individual[5]
-#         windWP_ind = individual[6]
-#         solarWP_ind = individual[7]
-
-#         LWSP_print = LWSP()
-#         LPSP_print = LPSP()
-
-#         # Convert chart images to base64 strings (dummy data for now)
-#         chart1 = b64encode(b'sample_chart_data1').decode('utf-8')
-#         chart2 = b64encode(b'sample_chart_data2').decode('utf-8')
-#         chart3 = b64encode(b'sample_chart_data3').decode('utf-8')
-#         chart4 = b64encode(b'sample_chart_data4').decode('utf-8')
-#         chart5 = b64encode(b'sample_chart_data5').decode('utf-8')
-
-#         # Store the calculated data in the session
-#         results = {
-#             "solar_energy": solar_energy, "solar": solar,
-#             "wind": wind, "wind_energy": wind_energy,
-#             "hydro": hydro, "hydro_energy": hydro_energy,
-#             "bio": bio, "bio_energy": bio_energy,
-#             "LPSP": LPSP_print, "LWSP": LWSP_print,
-#             "individual": individual,
-#             "solar_ind": solar_ind, "wind_ind": wind_ind, 'hydropower_ind': hydropower_ind,
-#             "biogas_ind": biogas_ind, "battery_ind": battery_ind,
-#             "biogasWP_ind": biogasWP_ind, "windWP_ind": windWP_ind, "solarWP_ind": solarWP_ind,
-#             "chart1": chart1, "chart2": chart2, "chart3": chart3, "chart4": chart4, "chart5": chart5
-#         }
-        
-        
-        
-
-       
-
-#         request.session['results'] = results
-
-#         # Send JSON response indicating the calculation is done
-#         return JsonResponse({"status": "ok", "url": "results_page"})
-
-#     return render(request, 'optimization_tool.html')
 
 
 def operations_view(request):
--- storefront/urls.py
+++ storefront/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 #importing the functions from other modules urls will take the user to
 from django.contrib import admin
 from django.urls import path, include
-from pages.views import home_view, about_view, optimization_view, operations_view, plot_view, calculations_view, results_view, demographic_view, your_form_submission_view
+from pages.views import home_view, about_view, optimization_view, operations_view, plot_view, calculations_view, results_view, demographic_view, your_form_submission_view, comparison
 
 #set up for where each url takes the user
 urlpatterns = [
@@ -30,6 +30,7 @@ urlpatterns = [
     path('optimization_tool/optimization', calculations_view, name = 'optimization'), #shows results
     path('demographic/', demographic_view, name='demographic'), #shows demographic data
     path('form_submission/', your_form_submission_view, name='your_form_submission_view'),
+    path('comparison/', comparison, name='comparison'),
    
 
 ]
